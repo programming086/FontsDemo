3e1c498a9b98417532fa43f76e771028d3434ccd
Игорь Малеваный <gethwalk@gmail.com>
2016-02-19 21:11:02 +0200

    Initial Commit

---
+++ FontsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FontsDemo
+//
+//  Created by Игорь on 19.02.16.
+//  Copyright © 2016 Ihor Malovanyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
