c92c1854a1a59fc7a817d2f3f203689df65ff137
Brovko Roman <brovko-roman@ya.ru>
2016-02-21 11:49:14 +0200

    add .gitignore and files

--- FontsDemo/ViewController.swift
+++ FontsDemo/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textDemo: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textDemo.font = UIFont(name: "NexaRustHandmade-Extended", size: 30)
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
